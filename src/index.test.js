d94df3155fbb0b72d1ce8653a186721095c025ea
adamscarlat <adamscarlat@Adams-MBP-2.fios-router.home>
2018-04-14 11:05:05 -0400

    Added a JSDOM example test

--- src/index.test.js
+++ src/index.test.js
@@ -1,4 +1,6 @@
 import {expect} from 'chai';
+import jsdom from 'jsdom';
+import fs from 'fs' //file system 
 
 //describes what the file is testing
 describe('First Test', () => {
@@ -6,5 +8,18 @@ describe('First Test', () => {
 	//explains what this specific unit-test is testing
 	it('should always pass', () => {
 		expect(true).to.equal(true);
+	})
+})
+
+
+describe('index.html', () => {
+	it('should say Its working', (done) => {
+		const index = fs.readFileSync('./src/index.html', 'utf-8');
+		jsdom.env(index, (err, window) => {
+			const h1 = window.document.getElementsByTagName('h1')[0];
+			expect(h1.innerHTML).to.equal("It's Working!");
+			done(); //marks the end of the async test
+			window.close();
+		})
 	})
 })
