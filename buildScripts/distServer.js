9b444d6a3cb3e9a0479557a1bdcc0df9b120c860
Scarlat <adam.scarlat@philips.com>
2018-04-16 10:18:20 -0400

    Separated client from API. API is now hosted on Heroku and clients is pointing to it

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -11,15 +11,7 @@ const app = express();
 app.use(express.static('dist'));
 app.use(compression());
 
-app.get('/users', function(req, res){
-	res.json([
-		{"id": 1,"firstName":"Bob", "lastName":"Smith", "email":"bob@gmail.coms"},
-		{"id": 2,"firstName":"Tammy","lastName":"Norton","email":"tnorton@yahoo.com"},
-		{"id": 3,"firstName":"Tina","lastName":"Lee","email":"lee.tina@hotmail.com"}
-	])
-});
-
-app.get('*', function(req, res) {
+app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,5 +1,5 @@
 export default function getBaseUrl() {
-  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : 'https://shrouded-springs-50598.herokuapp.com/';
 }
 
 function getQueryStringParameterByName(name, url) {
